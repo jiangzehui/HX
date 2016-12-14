e6fde56c53b39bc318a90979f55de9c2475d2556
jiangzehui <383001264@qq.com>
2016-12-14 16:57:43 +0800

    初始化环信SDK

---
+++ client/app/src/main/java/cn/jiangzehui/hx/MyApplication.java
@@ -0,0 +1,24 @@
+package cn.jiangzehui.hx;
+
+import android.app.Application;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
+/**
+ * Created by quxianglin on 16/12/14.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EMOptions options = new EMOptions();
+        // 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+        //初始化
+        EMClient.getInstance().init(getApplicationContext(), options);
+//在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
+    }
+}
